7c2e302cf6b0713ddc072f9ecac3e0eaa8d549d1
Harvey Huang <kyo6108@gmail.com>
2021-07-14 01:37:21 +0800

    first commit

---
+++ js/fourm0614.js
@@ -0,0 +1,4 @@
+var btn1 = document.getElementById('btn')
+btn1.addEventListener('click',function(){
+     window.open("https://ckeditor.com/");
+})  
---
+++ js/fourm0620.js
@@ -0,0 +1,16 @@
+var firstLi = document.querySelectorAll("header ul li");
+for(var i=0;i<firstLi.length;i++){
+    firstLi[i].onmouseover=function(){
+        if(this.querySelector("ul")){
+            var secUL=this.querySelector("ul");
+            var liLength=secUL.querySelectorAll('li').length;
+            secUL.style.height=38*liLength+'px';
+        }
+    }
+    firstLi[i].onmouseout=function(){
+        if(this.querySelector("ul")){
+            var secUL=this.querySelector("ul");
+            secUL.style.height='0px';
+        }
+    }
+}
